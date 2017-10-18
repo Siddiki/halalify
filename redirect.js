9a2cbc371aea11b683e2f9e7daec7502405535c3
Siddiki <siddiki@cooper.edu>
2017-10-18 15:37:10 -0400

    Skeleton code and some functions

--- redirect.js
+++ redirect.js
@@ -1,3 +1,35 @@
 // detect if website is naughty
 
+var API_KEY = "cb8f3c674efccfb2d758dd44d94955c7b32079b8";
+
+// get URL from click or on request
+function getUrl {
+// hamza todo
+}
+
+// make the API request
+function makeRequest(url) {
+    var request = "http://api.mywot.com/0.4/public_link_json2?hosts=".concat(url, "&callback=process&key=", API_KEY);
+    console.log(request);
+    return request;
+}
+
+// call the API
+function callAPI(request) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", request, false ); // false for synchronous request
+    xmlHttp.send( null );
+    var response = xmlHttp.responseText;
+    console.log(response);
+    return response;
+}
+
+// parse response for 401 adult content flag
+function isFlagged {
+//Abdullah todo
+}
+
 // redirect the page
+function redirect {
+// hamza todo
+}
